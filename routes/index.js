4fbf93b93cb8c59ffb1b6cb4bc58ef18d2f8ec20
Lisa Pan <l.pan4952@gmail.com>
2016-11-21 20:34:28 -0500

    hey chloe

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,48 @@ const Restaurant = models.Restaurant;
 module.exports = router;
 
 router.get('/', function (req, res, next) {
-  const findingHotels = Hotel.findAll({});
-  const findingActivities = Activity.findAll({});
-  const findingRestaurants = Restaurant.findAll({});
+  const findingHotels = Hotel.findAll({
+    order: 'name ASC'
+  });
+  const findingActivities = Activity.findAll({
+    order: 'name ASC'
+  });
+  const findingRestaurants = Restaurant.findAll({
+    order: 'name ASC'
+  });
+
+  const findingSelectedHotel = Hotel.findOne({
+    where: {
+      dayId: 1
+    }
+  });
+
+  const findingSelectedRestaurants = Restaurant.findAll({
+    where:{
+      dayId: 1
+    }
+  });
+
+  const findingSelectedActivities = Activity.findAll({
+    where:{
+      dayId: 1
+    }
+  });
   const findingAllTripOptions =
-  Promise.all([findingActivities, findingHotels, findingRestaurants])
+  Promise.all([findingActivities, 
+               findingHotels, 
+               findingRestaurants,
+               findingSelectedHotel,
+               findingSelectedRestaurants,
+               findingSelectedActivities])
           .then(function (data) {
             res.render('index', {
               templateHotels: data[1],
               templateRestaurants: data[2],
-              templateActivities: data[0]
+              templateActivities: data[0],
+              selectedHotel: data[3],
+              selectedRestaurants: data[4],
+              selectedActivities: data[5]
             })
           })
           .catch(next);
